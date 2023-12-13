bee3aa50ab48c9e6903ebbe840c783736d529386
大熊熊 <s86390474@yahoo.com>
2023-12-13 13:23:52 +0800

    update aabb

--- src/calculator.ts
+++ src/calculator.ts
@@ -4,7 +4,7 @@
  * @param b - 第二個數字
  */
 export function add(a: number, b: number): number {
-    // 在此實現函式
+     return a+b;
 }
 
 /**
@@ -14,6 +14,7 @@ export function add(a: number, b: number): number {
  */
 export function subtract(a: number, b: number): number {
     // 在此實現函式
+    return a-b;
 }
 
 /**
@@ -23,6 +24,7 @@ export function subtract(a: number, b: number): number {
  */
 export function multiply(a: number, b: number): number {
     // 在此實現函式
+    return a*b;
 }
 
 /**
@@ -32,6 +34,7 @@ export function multiply(a: number, b: number): number {
  */
 export function divide(a: number, b: number): number {
     // 在此實現函式
+    return a/b;
 }
 
 /**
